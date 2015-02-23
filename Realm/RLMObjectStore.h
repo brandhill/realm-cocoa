e2dd056426809d3ce7f3fb8a917b60425eb0328e
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-23 12:54:26 -0800

    [RLMUtil] Make sure swift list properties are initialized

--- Realm/RLMObjectStore.h
+++ Realm/RLMObjectStore.h
@@ -99,6 +99,7 @@ RLMObjectBase *RLMCreateObjectAccessor(__unsafe_unretained RLMRealm *realm,
                                        __unsafe_unretained RLMObjectSchema *objectSchema,
                                        NSUInteger index);
 
+void RLMInitializeSwiftListAccessor(RLMObjectBase *object);
 
 #ifdef __cplusplus
 }
